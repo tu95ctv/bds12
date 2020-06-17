e104e4b7d10f998127ffc68c79ccec5c4e189f73
Duc Tu <nguyenductu@gmail.com>
2020-06-17 22:13:25 +0700

    â

--- bds/models/loop_fetch_cron.py
+++ bds/models/loop_fetch_cron.py
@@ -31,18 +31,18 @@ class loop_fetch_cron(models.Model):
                 # loop_fetch_cron_id.fetch_current_id = fetch_id.id
                 # print ('end fetch trong loop')
 
-                # try:
-                #     print ('fetch trong loop')
-                #     fetch_id.fetch_all_url()
-                #     loop_fetch_cron_id.fetch_current_id = fetch_id.id
-                #     print ('end fetch trong loop')
-                # except Exception as e:
-                #     print ('co 1 loi')
-                #     try:
-                #         self.env['bds.error'].create({'n`ame':e, 'des': 'id:%s - name:%s'%(fetch_id.id, fetch_id.name)})
-                #     except Exception as e:
-                #         self.env['bds.error'].create({'name':'có một lỗi khi fetch', 'des': 'id:%s - name:%s'%(fetch_id.id, fetch_id.name)})
-                #     print ('end co 1 loi')
+                try:
+                    print ('fetch trong loop')
+                    fetch_id.fetch_all_url()
+                    loop_fetch_cron_id.fetch_current_id = fetch_id.id
+                    print ('end fetch trong loop')
+                except Exception as e:
+                    print ('co 1 loi')
+                    try:
+                        self.env['bds.error'].create({'name':'lỗi: %s trong fetch_cron'%e, 'des': 'id:%s - name:%s'%(fetch_id.id, fetch_id.name)})
+                    except Exception as e:
+                        self.env['bds.error'].create({'name':'có một lỗi khi fetch', 'des': 'id:%s - name:%s'%(fetch_id.id, fetch_id.name)})
+                    print ('end co 1 loi')
             else:
                 raise ValueError('khong ton tai: fetch_ids')
         else:
